acde95ad88c26871ffacb569465e37007068a140
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-06-10 18:55:01 +0500

    docker try 3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,11 +11,17 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      output:{
-        manualChunks(id) {
-          if (id.match('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        }
+      output: {
+        manualChunks(id: string) {
+          if (id.indexOf('node_modules') !== -1) {
+            const basic = id.toString().split('node_modules/')[1];
+            const sub1 = basic.split('/')[0];
+            if (sub1 !== '.pnpm') {
+              return sub1.toString();
+            }
+            const name2 = basic.split('/')[1];
+            return name2.split('@')[name2[0] === '@' ? 1 : 0].toString();
+          }
         }
       }
     }
