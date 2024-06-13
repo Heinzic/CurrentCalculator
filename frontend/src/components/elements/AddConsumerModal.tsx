52596bad65af9a127282c4e004dcb5cb9df53e31
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-06-13 16:16:30 +0500

    disable button fix

--- frontend/src/components/elements/AddConsumerModal.tsx
+++ frontend/src/components/elements/AddConsumerModal.tsx
@@ -19,7 +19,7 @@ interface IConsumerForm {
 
 function AddConsumerModal({active, setActive, sections}: CreateObjectProps) {
     
-    const {register, watch, reset} = useForm<IConsumerForm>({mode:'onBlur', })
+    const {register, watch, reset, formState} = useForm<IConsumerForm>({mode:'onBlur', })
     const watchAll = watch()
     const types = useGetConsumerTypesQuery()
     const [createConsumer] = useCreateConsumerMutation()
@@ -46,19 +46,19 @@ function AddConsumerModal({active, setActive, sections}: CreateObjectProps) {
                         <div className="flex flex-col px-[38px] gap-[10px] pt-[41px] bg-[#FFFFFF] rounded-md text-black">
                             <h1 className="text-[32px]">Создание потребителя</h1>
                             <input {...register('name', {required: true})} type="text" placeholder="Имя потребителя*" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
-                            <select {...register('type', {required:true})} className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl">
+                            <select {...register('type')} className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl">
                                 {types.data?.map(e => (
                                     <option value={e.id}>{e.name}</option>
                                 ))}
                             </select>
-                            <select {...register('section', {required:true})} className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl">
+                            <select {...register('section')} className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl">
                                 {sections.map(e => (
                                     <option value={e.id}>{e.name}</option>
                                 ))}
                             </select>
-                            <input {...register('volume', {required:true})} type="number" placeholder="Количество" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
-                            <input {...register('power_per_unit', {required:true})} type="number" placeholder="Мощность на единицу" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
-                            <input {...register('coefficient_demand', {required:true})} type="number" placeholder="Коэф. спроса" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
+                            <input {...register('volume')} type="number" placeholder="Количество" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
+                            <input {...register('power_per_unit')} type="number" placeholder="Мощность на единицу" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
+                            <input {...register('coefficient_demand')} type="number" placeholder="Коэф. спроса" className="py-[6px] px-[25px] flex-grow bg-[#EBEBEB] rounded-md text-xl"/>
                             <div className="flex justify-end gap-[22px] pb-[40px] mt-[41px] items-center text-black">
                                 <button onClick={(e) => {
                                     e.preventDefault()
@@ -66,7 +66,7 @@ function AddConsumerModal({active, setActive, sections}: CreateObjectProps) {
                                 }} className="bg-[#D0D4D9] px-[18px] py-[10px] rounded-md flex-grow max-w-[241px] text-center border-[1px] hover:border-gray-700">
                                     Отмена                                
                                 </button>
-                                <button type="button" onClick={handleSubmit} className="bg-[#9AA8B0] px-[18px] py-[10px] rounded-md flex-grow max-w-[241px] text-center border-[1px] hover:border-gray-700">
+                                <button type="button" disabled={!formState.isDirty || !formState.isValid} onClick={handleSubmit} className="bg-[#9AA8B0] disabled:bg-[#bce4f0] px-[18px] py-[10px] rounded-md flex-grow max-w-[241px] text-center border-[1px] hover:border-gray-700">
                                     Сохранить                                
                                 </button>
                             </div>
